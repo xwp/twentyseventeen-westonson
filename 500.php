1c943d0c9f8ab001a843e88e3f44bd7db3863722
Shannon MacMillan <shannon@bluestormcreative.com>
2018-11-06 15:07:52 -0700

    500 error page markup

--- 500.php
+++ 500.php
@@ -15,10 +15,11 @@ get_header(); ?>
 
 			<section class="error-500 internal-server-error">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! Something went wrong.', 'twentyseventeen-westonson' ); ?></h1>
+					<img class="page-header-image" src="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/500-image.png'; ?>" alt="A bronze compass sitting on a stone surface with a waterfall in the distance'" />
+					<h1 class="page-title"><?php esc_html_e( 'Internal Service Error', 'twentyseventeen-westonson' ); ?></h1>
 				</header><!-- .page-header -->
 				<div class="page-content">
-					<p><?php esc_html_e( 'Something prevented the page from being rendered. Please try again.', 'twentyseventeen-westonson' ); ?></p>
+					<p><?php esc_html_e( 'Oops, something went wrong. Please try refreshing the page.', 'twentyseventeen-westonson' ); ?></p>
 
 					<?php
 					if ( function_exists( 'wp_service_worker_error_details_template' ) ) {
